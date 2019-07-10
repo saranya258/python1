b27dc8a073ead6a1785851ea64fc616ab04b9948
K.saranya <51949493+saranya258@users.noreply.github.com>
2019-07-10 16:39:43 +0530

    Create 18pro.py

---
+++ 18pro.py
@@ -0,0 +1,12 @@
+import math
+saran,pavi=map(int,input().split())
+Sri=[]
+loga=list(map(int,input().split()))
+for i in range(0,pavi):
+	l,h=map(int,input().split())
+Sri.append([l,h])
+for i in Sri:
+	kk=i[0]-1
+  ll=i[1]-1
+print(math.gcd(loga[kk],loga[ll]))
+
